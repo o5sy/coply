1aa7f99e252f6722e69ee4248ec92c18865d579a
o-seung-yeon <worko5sy@gmail.com>
2024-07-15 19:29:54 +0900

    Feat: title 태그 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,13 @@
 import '@/styles/globals.css';
 
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Head>
+      <title>Coply</title>
+      <Component {...pageProps} />
+    </Head>
+  );
 }
