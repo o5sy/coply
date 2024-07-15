7d96f7c1c6c3b42c6484c787a5760233ef3301e1
o5sy <oosydev@gmail.com>
2024-07-16 00:08:39 +0900

    Chore: tailwind color 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,5 +3,13 @@ import type { Config } from 'tailwindcss';
 const config: Config = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   plugins: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#529480',
+        secondary: '#E8F2F0',
+      },
+    },
+  },
 };
 export default config;
