1028d96bb97d320460440e7f98dd722a1da67d99
o5sy <oosydev@gmail.com>
2024-07-21 23:14:51 +0900

    Design: 페이지 레이아웃 큰틀 잡기

--- src/pages/explore/index.tsx
+++ src/pages/explore/index.tsx
@@ -1,8 +1,12 @@
+import Head from 'next/head';
 import { Pagination, VideoList } from '@/components/explore-page';
 import { Header } from '@/components/header';
-import Head from 'next/head';
+import { SearchInput } from '@/components/header/search-input';
+import { Separator } from '@/components/shared';
 
 export default function ExplorePage() {
+  const keyword = '';
+
   return (
     <>
       <Head>
@@ -13,13 +17,29 @@ export default function ExplorePage() {
         <Header />
         <main className="absolute top-0 mb-[100px] mt-[65px] w-full">
           <div className="layout">
-            <section className="flex flex-col items-center justify-center">
-              {/* 영상 리스트 */}
-              <VideoList />
+            <div className="flex items-center justify-between py-[32px]">
+              <h1 className="text-3xl font-bold">영상 탐색</h1>
+              <SearchInput />
+            </div>
 
-              {/* 페이지네이션 */}
-              <Pagination totalPage={101} />
-            </section>
+            <div className="flex w-full gap-[24px]">
+              <aside className="flex w-[300px] flex-col gap-[10px]">
+                <div className="text-xl">필터</div>
+                <Separator className="border-black" />
+              </aside>
+
+              <div className="w-full">
+                <div className="pb-[24px] text-xl">
+                  {keyword ? `"${keyword}" 검색 결과` : '10건의 영상'}
+                </div>
+                <section className="flex w-full flex-col items-center justify-center">
+                  {/* 영상 리스트 */}
+                  <VideoList />
+                  {/* 페이지네이션 */}
+                  <Pagination totalPage={101} />
+                </section>
+              </div>
+            </div>
           </div>
         </main>
       </div>
