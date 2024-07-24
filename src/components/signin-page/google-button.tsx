24b65007b6063d0943294531e167342aa7c82c66
o5sy <oosydev@gmail.com>
2024-07-25 01:09:55 +0900

    Feat: 구글 로그인 기능 추가

--- src/components/signin-page/google-button.tsx
+++ src/components/signin-page/google-button.tsx
@@ -8,13 +8,15 @@ const roboto = Roboto({
 
 interface GoogleButtonProps {
   onClick: () => void;
+  disabled?: boolean;
 }
-export function GoogleButton({ onClick }: GoogleButtonProps) {
+export function GoogleButton({ onClick, disabled }: GoogleButtonProps) {
   return (
     <button
       className="flex h-[40px] items-center gap-[24px] rounded-sm bg-white px-[8px] shadow-lg"
       type="button"
       onClick={onClick}
+      disabled={disabled}
     >
       <Image src="/google.svg" alt="google" width={18} height={18} />
       <span className={`text-[14px] text-[#000]/[.54] ${roboto.className}`}>
