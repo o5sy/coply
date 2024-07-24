3ef81546c4b029bd601ba915faa3fc7ec1751b73
o5sy <oosydev@gmail.com>
2024-07-25 01:09:16 +0900

    Feat: signIn api 리턴 타입 추가

--- src/apis/auth.ts
+++ src/apis/auth.ts
@@ -1,7 +1,13 @@
 import { axiosInstance } from './axios';
-import { SignInRequest, signInResponseSchema } from './models/auth';
+import {
+  SignInRequest,
+  SignInResponse,
+  signInResponseSchema,
+} from './models/auth';
 
-export const signIn = async (request: SignInRequest) => {
+export const signIn = async (
+  request: SignInRequest,
+): Promise<SignInResponse> => {
   const { data } = await axiosInstance.post('/auth/login', request);
   const response = signInResponseSchema.parse(data);
   return response;
