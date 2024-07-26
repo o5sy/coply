ea9653e88463aab73a6867e7b824cecafad70ce2
seungyeon <oosydev@gmail.com>
2024-07-26 11:39:57 +0900

    [Fix] 프로덕션에서 api 요청시 mixed content 오류 발생하는 이슈 수정 (#16)
    Fix: mixed content 오류 수정을 위해 메타 태그 추가

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,12 @@ import { Head, Html, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="ko">
-      <Head title="Coply" />
+      <Head title="Coply">
+        <meta
+          httpEquiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
