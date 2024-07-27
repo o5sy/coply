5986d6544fd4d76c98d316ef32e97ebdb384ddfa
o5sy <oosydev@gmail.com>
2024-07-27 21:20:49 +0900

    Refactor: 페이지 컴포넌트에서 컨텐츠를 컨테이너 컴포넌트로 분리

--- src/pages/watch/index.tsx
+++ src/pages/watch/index.tsx
@@ -1,19 +1,7 @@
 import Head from 'next/head';
-import Link from 'next/link';
-import YouTube from 'react-youtube';
-import {
-  PopoverBackdrop,
-  UserMenu,
-  VideoInfoSection,
-  WatchPageHeader,
-} from '@/components/watch-page';
-import { useOpenState } from '@/hooks';
+import { WatchContainer } from '@/containers/pages/watch';
 
 export default function WatchPage() {
-  const { isOpen: isOpenDrawer, handleState: handleDrawer } = useOpenState();
-  const { isOpen: isOpenUserMenu, handleState: handleUserMenu } =
-    useOpenState();
-
   const title = '영상 제목';
 
   return (
@@ -22,58 +10,7 @@ export default function WatchPage() {
         <title>{title}</title>
       </Head>
 
-      <div
-        className={`h-[100vh] w-[100vw] ${isOpenDrawer || isOpenUserMenu ? 'relative' : ''}`}
-      >
-        <div className="flex h-full w-full flex-col">
-          {/* header */}
-          <WatchPageHeader
-            title={title}
-            onDrawerMenu={handleDrawer.open}
-            onUserMenu={handleUserMenu.open}
-          />
-
-          {/* player */}
-          <main className="flex-center flex-1 bg-gray-700">
-            <YouTube
-              videoId="M7lc1UVf-VE"
-              opt={{
-                height: '360',
-                width: '640',
-              }}
-              className="h-full w-full"
-              iframeClassName="h-[inherit] w-[inherit]"
-            />
-          </main>
-
-          {/* bottom */}
-          <div className="flex h-[60px] items-center bg-gray-600 px-[24px]">
-            <Link href="/" className="px-[4px] py-[8px] text-sm text-gray-300">
-              &lt; 홈으로 이동
-            </Link>
-          </div>
-        </div>
-
-        {/* drawer menu */}
-        {isOpenDrawer && (
-          <PopoverBackdrop className="bg-black/70" onClick={handleDrawer.close}>
-            <VideoInfoSection
-              title={title}
-              onClose={handleDrawer.close}
-              channelName="Coply"
-              description="설명"
-              uploadedAt="2022. 5. 1."
-            />
-          </PopoverBackdrop>
-        )}
-
-        {/* user menu */}
-        {isOpenUserMenu && (
-          <PopoverBackdrop onClick={handleUserMenu.close}>
-            <UserMenu className="absolute right-[24px] top-[60px]" />
-          </PopoverBackdrop>
-        )}
-      </div>
+      <WatchContainer title={title} />
     </>
   );
 }
