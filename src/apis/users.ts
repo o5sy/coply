be7c11da3603929fa633b8f5dcbfec5726b4cdf3
o5sy <oosydev@gmail.com>
2024-07-29 00:58:46 +0900

    Feat: 시청 기록 조회 api fetcher 추가

--- src/apis/users.ts
+++ src/apis/users.ts
@@ -1,8 +1,26 @@
 import { axiosInstance } from './axios';
+import {
+  GetViewingHistoryResponse,
+  getViewingHistoryResponseSchema,
+} from './models/user';
 
 export const getUser = async (token: string) => {
   const { data } = await axiosInstance.get('/users/me', {
     headers: { Authorization: `Bearer ${token}` },
   });
   return data;
+};
+
+export const getViewingHistoriesByVideoId = async (
+  videoId: string,
+  accessToken: string,
+): Promise<GetViewingHistoryResponse> => {
+  const { data } = await axiosInstance.get(
+    `/users/viewing-histories/${videoId}`,
+    {
+      headers: { Authorization: `Bearer ${accessToken}` },
+    },
+  );
+  const viewingHistories = getViewingHistoryResponseSchema.parse(data);
+  return viewingHistories;
 };
