3c23fda1ec6e246421de112865256c4dd45111a4
o5sy <oosydev@gmail.com>
2024-07-30 11:43:15 +0900

    Feat: 작업했던 코드 로그 남기기용 커밋 (wip)

--- src/containers/pages/watch/watch.container.tsx
+++ src/containers/pages/watch/watch.container.tsx
@@ -1,14 +1,27 @@
 import {
+  getViewingHistoriesByVideoId,
+  updateViewingHistoryByVideoId,
+} from '@/apis/users';
+import {
   PopoverBackdrop,
   UserMenu,
   VideoInfoSection,
   WatchPageHeader,
 } from '@/components/watch-page';
-import { useOpenState } from '@/hooks';
+import { ACCESS_TOKEN } from '@/constants/local-storage-key';
+import { useDebounce, useLocalStorage, useOpenState } from '@/hooks';
 import { getFormattedDate } from '@/utils/date.util';
+import { getSession } from '@/utils/session';
+import { skipToken, useMutation, useQuery } from '@tanstack/react-query';
 import Link from 'next/link';
 import YouTube from 'react-youtube';
 import { useGetVideoByIdQuery } from './hooks';
+import { UpsertViewingHistoryRequestParams } from '@/apis/models/user';
+import { axiosInstance } from '@/apis/axios';
+import { useCallback, useEffect, useMemo, useRef } from 'react';
+// import { throttle } from '@/utils/event-handling';
+import { debounce, throttle } from 'lodash';
+import { useDebouncedInterval } from './hooks/use-debounced-interval';
 
 export function WatchContainer() {
   const { data: video } = useGetVideoByIdQuery();
@@ -16,6 +29,106 @@ export function WatchContainer() {
   const { isOpen: isOpenDrawer, handleState: handleDrawer } = useOpenState();
   const { isOpen: isOpenUserMenu, handleState: handleUserMenu } =
     useOpenState();
+
+  const [accessToken] = useLocalStorage(ACCESS_TOKEN, getSession());
+
+  const { data } = useQuery({
+    queryKey: ['viewing-histories', { videoId: video?.id }],
+    queryFn:
+      video?.id && accessToken
+        ? () => {
+            return getViewingHistoriesByVideoId(video.id, accessToken);
+          }
+        : skipToken,
+    enabled: !!accessToken,
+  });
+
+  // onLoad 시 data 있거나 data 있을 때 플레이어 시작 시간 이동
+  // todo 영상 시작, 일시정지, 주기마다 -> 디바운싱해서 api 호출
+
+  const { mutate } = useMutation({
+    mutationFn: ({
+      videoId,
+      accessToken,
+      params,
+    }: {
+      videoId: string;
+      accessToken: string;
+      params: UpsertViewingHistoryRequestParams;
+    }) => {
+      return updateViewingHistoryByVideoId(videoId, accessToken, params);
+    },
+  });
+
+  // const intervalIdRef = useRef<NodeJS.Timeout | null>(null);
+
+  const debounced = useDebounce(() => {
+    console.log('debounced');
+  }, 2000);
+
+  // const i = useCallback(
+  //   debounce(() => {
+  //     setInterval(() => {
+  //       console.log('debounced interval');
+  //     }, 2000);
+  //   }),
+  //   [],
+  // );
+
+  // const a = () => {
+  //   let timeout: NodeJS.Timeout | null = null;
+
+  //   const startDebouncedInterval = (callbackFn: () => void) => {
+  //     return debounce(() => {
+  //       timeout = setInterval(() => {
+  //         callbackFn();
+  //       }, 2000);
+  //     });
+
+  //     // debounce 를 리턴해서 실행시켜야 내부 로직이 동작함
+  //   };
+
+  //   const clearDebouncedInterval = () => {
+  //     if (!timeout) {
+  //       return;
+  //     }
+  //     clearInterval(timeout);
+  //     timeout = null;
+  //   };
+
+  //   return [startDebouncedInterval, clearDebouncedInterval] as const;
+  // };
+
+  // const [start, clear] = useMemo(() => a(), []);
+
+  const { startDebouncedInterval, clearDebouncedInterval } =
+    useDebouncedInterval(2000);
+
+  // const startInterval = useCallback(() => {
+  //   if (intervalIdRef.current) {
+  //     return;
+  //   }
+  //   const i = setInterval(() => {
+  //     console.log('throttling');
+  //   }, 2000);
+  //   // console.log('interval', i);
+  //   intervalIdRef.current = i;
+  // }, [debounced]);
+
+  // const cancelInterval = () => {
+  //   if (!intervalIdRef.current) {
+  //     return;
+  //   }
+  //   // console.log('clear Interval', intervalIdRef.current);
+  //   clearInterval(intervalIdRef.current);
+  //   intervalIdRef.current = null;
+  // };
+
+  // useEffect(() => {
+  //   return () => {
+  //     clear();
+  //   };
+  // }, []);
 
   return (
     <div
@@ -34,12 +147,39 @@ export function WatchContainer() {
           {video && (
             <YouTube
               videoId={video.id}
-              opt={{
-                height: '360',
+              opts={{
                 width: '640',
+                height: '360',
               }}
               className="h-full w-full"
               iframeClassName="h-[inherit] w-[inherit]"
+              onReady={() => {
+                // todo 시간 이동
+                // console.log('load');
+              }}
+              onStateChange={({ data, target }) => {
+                // 0, 2 일 때 시간 저장 (종료, 일시중지)
+                // 1 일 때 setInterval 추가
+                // 이거 세개 묶어서 디바운스 걸어주기
+                console.log('onStateChange', data);
+                if (
+                  data === YouTube.PlayerState.ENDED ||
+                  data === YouTube.PlayerState.PAUSED
+                ) {
+                  // console.log(target.getCurrentTime());
+                  debounced();
+                  // cancelInterval();
+                  clearDebouncedInterval();
+                }
+
+                if (data === YouTube.PlayerState.PLAYING) {
+                  // throttled();
+                  // startInterval();
+                  startDebouncedInterval(() => console.log('new interval'));
+
+                  // setTimeout;
+                }
+              }}
             />
           )}
         </main>
