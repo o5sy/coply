4789640d58978f050c56ac26f3b27788faa4c884
o5sy <oosydev@gmail.com>
2024-07-30 22:21:32 +0900

    Feat: 로그인 상태 캐싱되도록 useQuery 로 호출하도록 수정

--- src/apis/models/user.ts
+++ src/apis/models/user.ts
@@ -1,6 +1,10 @@
 import { z } from 'zod';
 import { videoSchema } from './video';
 
+export const getUserResponseSchema = z.object({
+  email: z.string(),
+});
+
 export const getViewingHistoryResponseSchema = z.object({
   duration: z.number(),
   video: videoSchema,
@@ -9,6 +13,8 @@ export const getViewingHistoryResponseSchema = z.object({
 export const UpsertViewingHistoryRequestParamsSchema = z.object({
   duration: z.number(),
 });
+
+export type GetUserResponse = z.infer<typeof getUserResponseSchema>;
 
 export type GetViewingHistoryResponse = z.infer<
   typeof getViewingHistoryResponseSchema
