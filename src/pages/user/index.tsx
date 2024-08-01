011b7eb1cae32190dc2b56bc5bed8c96d5063337
o5sy <oosydev@gmail.com>
2024-08-01 11:26:09 +0900

    Refactor: 분리한 컴포넌트로 교체

--- src/pages/user/index.tsx
+++ src/pages/user/index.tsx
@@ -1,28 +1,12 @@
 import { ReactElement } from 'react';
-import Link from 'next/link';
-import { Button } from '@/components/shared';
-import { Guide } from '@/components/shared/guide';
 import { LayoutWithHeader } from '@/components/shared/layout-with-header';
 import { WithAuth } from '@/components/shared/with-auth';
+import { NoAuthGuide } from '@/components/user-page';
 import { UserContainer } from '@/containers/pages/user';
 
 export default function UserPage() {
   return (
-    <WithAuth
-      fallback={
-        <main className="layout w-full p-[16px] pt-[140px]">
-          <Guide
-            text="시청한 영상을 확인하세요"
-            subText="로그인을 통해 시청 중인 이력을 관리할 수 있습니다."
-            bottomContents={
-              <Link href="/signin">
-                <Button>로그인</Button>
-              </Link>
-            }
-          />
-        </main>
-      }
-    >
+    <WithAuth fallback={<NoAuthGuide />}>
       <UserContainer />;
     </WithAuth>
   );
