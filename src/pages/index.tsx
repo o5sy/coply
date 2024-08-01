96adca934ab293f84cfdb876ce818e57633c4a1e
o5sy <oosydev@gmail.com>
2024-08-01 21:01:55 +0900

    Feat: 메인 페이지 빌드 시 prefetch 하도록 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,10 @@
+import { GetStaticProps } from 'next';
+import { dehydrate, QueryClient } from '@tanstack/react-query';
 import { ReactElement } from 'react';
 import { LayoutWithHeader } from '@/components/shared/layout-with-header';
 import { MainContainer } from '@/containers/pages/main';
+import { getVideoById } from '@/apis/videos';
+import { recommendedSections } from '@/containers/pages/main/models/main.model';
 
 export default function MainPage() {
   return <MainContainer />;
@@ -9,3 +13,24 @@ export default function MainPage() {
 MainPage.getLayout = function getLayout(page: ReactElement) {
   return <LayoutWithHeader title="Coply">{page}</LayoutWithHeader>;
 };
+
+export const getStaticProps = (async () => {
+  const queryClient = new QueryClient();
+
+  const videoIds = recommendedSections.flatMap((section) => section.videoIds);
+
+  const requests = videoIds.map((videoId) => {
+    return queryClient.prefetchQuery({
+      queryKey: ['videos', { videoId }],
+      queryFn: () => getVideoById(videoId),
+    });
+  });
+
+  await Promise.allSettled(requests);
+
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+    },
+  };
+}) satisfies GetStaticProps;
