696c327c3dc74821923c25125d4d946e946e1609
o5sy <oosydev@gmail.com>
2024-08-01 11:22:45 +0900

    Feat: 유저 페이지 접근 시 미인증 상태일 경우 안내 ui 표시하도록 추가

--- src/pages/user/index.tsx
+++ src/pages/user/index.tsx
@@ -1,9 +1,31 @@
 import { ReactElement } from 'react';
+import Link from 'next/link';
+import { Button } from '@/components/shared';
+import { Guide } from '@/components/shared/guide';
 import { LayoutWithHeader } from '@/components/shared/layout-with-header';
+import { WithAuth } from '@/components/shared/with-auth';
 import { UserContainer } from '@/containers/pages/user';
 
 export default function UserPage() {
-  return <UserContainer />;
+  return (
+    <WithAuth
+      fallback={
+        <main className="layout w-full p-[16px] pt-[140px]">
+          <Guide
+            text="시청한 영상을 확인하세요"
+            subText="로그인을 통해 시청 중인 이력을 관리할 수 있습니다."
+            bottomContents={
+              <Link href="/signin">
+                <Button>로그인</Button>
+              </Link>
+            }
+          />
+        </main>
+      }
+    >
+      <UserContainer />;
+    </WithAuth>
+  );
 }
 
 UserPage.getLayout = function getLayout(page: ReactElement) {
