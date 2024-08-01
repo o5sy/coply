45499ba5436520fab40797903ec9a7f41bec238a
o5sy <oosydev@gmail.com>
2024-08-01 11:25:59 +0900

    Feat: 유저 페이지 미인증 시 가이드 <NoAuthGuide /> 추가

---
+++ src/components/user-page/no-auth-guide.tsx
@@ -0,0 +1,19 @@
+import Link from 'next/link';
+import { Button } from '../shared';
+import { Guide } from '../shared/guide';
+
+export function NoAuthGuide() {
+  return (
+    <main className="layout w-full p-[16px] pt-[140px]">
+      <Guide
+        text="시청한 영상을 확인하세요"
+        subText="로그인을 통해 시청 중인 이력을 관리할 수 있습니다."
+        bottomContents={
+          <Link href="/signin">
+            <Button>로그인</Button>
+          </Link>
+        }
+      />
+    </main>
+  );
+}
