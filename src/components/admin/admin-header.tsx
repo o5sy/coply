58af1878fa4bc4069d9aeb0329261a9f9a07dec3
o5sy <oosydev@gmail.com>
2024-08-04 23:43:41 +0900

    Design: <AdminHeader /> 추가

---
+++ src/components/admin/admin-header.tsx
@@ -0,0 +1,43 @@
+import Link from 'next/link';
+import { useState } from 'react';
+import { twMerge } from 'tailwind-merge';
+import { Logo } from '../header/logo';
+
+const tabs = ['video', 'roadmap'] as const;
+
+type Tab = (typeof tabs)[number];
+
+export function AdminHeader() {
+  const [selectedTab, setSelectedTab] = useState<Tab>('video');
+
+  return (
+    <header className="sticky left-0 top-0 z-10 w-full bg-white">
+      <div className="layout flex h-[65px] items-center">
+        {/* logo */}
+        <div className="flex items-start">
+          <Logo />
+          <span className="justify-self-start text-sm italic text-red-600">
+            Admin
+          </span>
+        </div>
+
+        {/* navigator */}
+        <nav className="flex flex-1 gap-4 px-[32px] py-2">
+          {tabs.map((tab) => (
+            <Link
+              key={tab}
+              className={twMerge(
+                'block rounded-lg px-3 py-2 capitalize hover:text-primary',
+                selectedTab === tab && 'bg-primary/25 font-bold text-primary',
+              )}
+              href={`/admin/${tab}`}
+              onClick={() => setSelectedTab(tab)}
+            >
+              {tab}
+            </Link>
+          ))}
+        </nav>
+      </div>
+    </header>
+  );
+}
