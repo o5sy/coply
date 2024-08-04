ae520f0a428df6cd1cef9206c1d3dec962706fd8
o5sy <oosydev@gmail.com>
2024-08-04 23:47:50 +0900

    Feat: Roadmap 페이지 컴포넌트에 header 추가

--- src/pages/admin/roadmap.tsx
+++ src/pages/admin/roadmap.tsx
@@ -1,3 +1,14 @@
+import { ReactElement } from 'react';
+import { LayoutWithAdminHeader } from '@/components/admin';
+
 export default function RoadmapPage() {
   return <div>roadmap</div>;
 }
+
+RoadmapPage.getLayout = function getLayout(page: ReactElement) {
+  return (
+    <LayoutWithAdminHeader title="로드맵 관리 | Coply">
+      {page}
+    </LayoutWithAdminHeader>
+  );
+};
