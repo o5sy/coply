b9e2de4ba7db9f49885d4e1ce259be2ae6efe49e
o5sy <oosydev@gmail.com>
2024-08-04 23:46:54 +0900

    Feat: Video 페이지 컴포넌트에 header 추가

--- src/pages/admin/video.tsx
+++ src/pages/admin/video.tsx
@@ -1,3 +1,14 @@
+import { ReactElement } from 'react';
+import { LayoutWithAdminHeader } from '@/components/admin';
+
 export default function VideoPage() {
   return <div>video</div>;
 }
+
+VideoPage.getLayout = function getLayout(page: ReactElement) {
+  return (
+    <LayoutWithAdminHeader title="영상 관리 | Coply">
+      {page}
+    </LayoutWithAdminHeader>
+  );
+};
