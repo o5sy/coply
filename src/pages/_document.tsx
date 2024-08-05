f02e85de8137c6c4ad157cccf450686c483002e3
o5sy <oosydev@gmail.com>
2024-08-06 01:07:57 +0900

    Comment: meta tag 주석 처리
    ssh 설정 후 삭제할 예정

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,10 +4,10 @@ export default function Document() {
   return (
     <Html lang="ko">
       <Head title="Coply">
-        <meta
+        {/* <meta
           httpEquiv="Content-Security-Policy"
           content="upgrade-insecure-requests"
-        />
+        /> */}
       </Head>
       <body>
         <Main />
