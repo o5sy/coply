488eb2931747ac6266993323982994ece969f038
o5sy <oosydev@gmail.com>
2024-08-06 23:33:23 +0900

    Feat: TableHeaderDef 타입 수정

--- src/components/admin/data-table/types/data-table.type.ts
+++ src/components/admin/data-table/types/data-table.type.ts
@@ -1,11 +1,17 @@
 import { ReactNode, HTMLAttributes } from 'react';
 
-export type TableHeaderDef = {
+export interface TableHeaderDef extends HTMLAttributes<HTMLTableCellElement> {
   key: string;
   contents?: ReactNode; // undefined 일 경우 key 값을 렌더링
-};
+}
 
 export interface TableRowDef extends HTMLAttributes<HTMLTableRowElement> {
   key: string;
-  columns: ReactNode[]; // undefined 일 경우 key 값을 렌더링
+  columns: ReactNode[];
 }
+
+// Cell 간 순서 변경 기능은 없어서 key 값으로 인덱스를 써도 문제는 없음
+// export interface TableColumnDef extends HTMLAttributes<HTMLTableCellElement> {
+//   key: string;
+//   contents: ReactNode;
+// }
