64b9b78c320961ce71cdfa101506dc1c2e6e7a48
o5sy <oosydev@gmail.com>
2024-08-07 00:11:43 +0900

    Feat: 영상 관리 리듀서 추가

---
+++ src/components/admin/add-video-dialog-content/reducers/manage-video.reducer.ts
@@ -0,0 +1,76 @@
+import { produce } from 'immer';
+import { uniqueId } from 'lodash';
+import { Reducer } from 'react';
+import { CategoryUnion, LevelUnion } from '@/apis/models/video';
+
+type ManageVideoState = {
+  id: string;
+  videoId: string;
+  category: CategoryUnion;
+  level: LevelUnion[];
+}[];
+
+type ManageVideoAction =
+  | { type: 'addItem' }
+  | { type: 'updateVideoId'; payload: { id: string; videoId: string } }
+  | { type: 'updateCategory'; payload: { id: string; category: CategoryUnion } }
+  | {
+      type: 'updateLevel';
+      payload: { id: string; level: LevelUnion; checked: boolean };
+    }
+  | { type: 'removeItem'; id: string };
+
+export const manageVideoReducer: Reducer<
+  ManageVideoState,
+  ManageVideoAction
+> = (prevState, action) => {
+  switch (action.type) {
+    case 'addItem': {
+      return produce(prevState, (draft) => {
+        draft.push({
+          id: uniqueId(),
+          videoId: '',
+          category: 'FE',
+          level: ['BEGINNER'],
+        });
+      });
+    }
+    case 'updateVideoId': {
+      return produce(prevState, (draft) => {
+        const index = draft.findIndex((item) => item.id === action.payload.id);
+        if (index !== -1) {
+          draft[index].videoId = action.payload.videoId;
+        }
+      });
+    }
+    case 'updateCategory':
+      return produce(prevState, (draft) => {
+        const index = draft.findIndex((item) => item.id === action.payload.id);
+        if (index !== -1) {
+          draft[index].category = action.payload.category;
+        }
+      });
+    case 'updateLevel':
+      return produce(prevState, (draft) => {
+        const index = draft.findIndex((item) => item.id === action.payload.id);
+        if (index !== -1) {
+          if (action.payload.checked) {
+            draft[index].level.push(action.payload.level);
+          } else {
+            draft[index].level = draft[index].level.filter(
+              (item) => item !== action.payload.level,
+            );
+          }
+        }
+      });
+    case 'removeItem':
+      return produce(prevState, (draft) => {
+        const index = draft.findIndex((todo) => todo.id === action.id);
+        if (index !== -1) {
+          draft.splice(index, 1);
+        }
+      });
+    default:
+      return prevState;
+  }
+};
