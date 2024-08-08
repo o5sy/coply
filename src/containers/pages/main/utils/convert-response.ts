1dece0feaf039a4aa1dbe6ec3da6853e29a62e53
o5sy <oosydev@gmail.com>
2024-08-09 02:01:56 +0900

    Fix: undefined 참조 오류 수정 (Record -> Map 타입으로 변경)
    - 이슈: 관리자 페이지에서 영상 삭제 테스트 후 메인 페이지 접근 시 undefined 참조 오류가 발생하면서 500 오류 리턴함
    
    - 원인:
    - 메인에 보여줄 추천 영상 id를 프론트에서 관리하고, 데이터는 서버에서 불러와서 바인딩 하고 있음
    - 받아온 데이터는 Record 타입으로, id 를 키값으로 참조하도록 했음
    - 프론트에선 하드코딩된 id 를 모두 순회하며 받아온 데이터를 조회하는데 Record 타입에 조회한 데이터가 없는 경우 reference error 가 발생함
    
    - 해결:
    Record 타입 대신 Map 타입을 사용해, 값이 있는 경우에만 변환해서 렌더링하도록 함
    이외에도 키값으로 접근해서 데이터를 참조하려고 하는 경우에는 객체가 아닌 Map 타입을 사용해 개발 단계에서 체크하려고 함

--- src/containers/pages/main/utils/convert-response.ts
+++ src/containers/pages/main/utils/convert-response.ts
@@ -3,15 +3,26 @@ import { VideoItem } from '@/components/shared/video-list';
 
 export const getVideoItems = (
   videoIds: string[],
-  videos: Record<string, GetVideoResponse>,
+  videos: Map<string, GetVideoResponse>,
 ): VideoItem[] => {
-  return videoIds.map((id) => {
-    const video = videos[id];
-    return {
+  if (!videos) {
+    return [];
+  }
+
+  const videoItems: VideoItem[] = [];
+
+  videoIds.forEach((id) => {
+    const video = videos.get(id);
+    if (!video) {
+      return;
+    }
+    videoItems.push({
       id: video.id,
       name: video.name,
       channelName: video.videoChannel.name,
       thumbnailUrl: video.thumbnailImageUrl,
-    };
+    });
   });
+
+  return videoItems;
 };
