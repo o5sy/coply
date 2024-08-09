ba219f86b450c730634c8c7cc0396c899ed00fc2
o5sy <oosydev@gmail.com>
2024-08-10 00:23:09 +0900

    Feat: 영상 추가 기능 추가

--- src/containers/pages/admin/video/containers/add-video-dialog-content-container/add-video-dialog-content.container.tsx
+++ src/containers/pages/admin/video/containers/add-video-dialog-content-container/add-video-dialog-content.container.tsx
@@ -1,5 +1,5 @@
-import { useReducer } from 'react';
-import * as DialogPrimitive from '@radix-ui/react-dialog';
+import { addVideoForAdmin } from '@/apis/admin';
+import { AddVideoForAdminRequestParams } from '@/apis/models/admin';
 import { AddVideoTable } from '@/components/admin/add-video-table';
 import { Button } from '@/components/shared';
 import {
@@ -7,6 +7,12 @@ import {
   DialogHeader,
   DialogTitle,
 } from '@/components/ui/dialog';
+import { ACCESS_TOKEN } from '@/constants/local-storage-key';
+import { useLocalStorage } from '@/hooks';
+import { getSession } from '@/utils/session';
+import * as DialogPrimitive from '@radix-ui/react-dialog';
+import { useMutation } from '@tanstack/react-query';
+import { useReducer } from 'react';
 import { manageVideoReducer } from './reducers';
 import { getInitialVideos } from './utils';
 
@@ -15,6 +21,38 @@ export function AddVideoDialogContentContainer() {
     manageVideoReducer,
     getInitialVideos(),
   );
+
+  const [accessToken] = useLocalStorage(ACCESS_TOKEN, getSession());
+
+  const { mutate } = useMutation({
+    mutationFn: accessToken
+      ? ({
+          accessToken,
+          params,
+        }: {
+          accessToken: string;
+          params: AddVideoForAdminRequestParams;
+        }) => addVideoForAdmin(accessToken, params)
+      : undefined,
+  });
+
+  const handleSave = () => {
+    if (!accessToken) {
+      return;
+    }
+    const videos = videoItems
+      .filter((item) => !!item.videoId)
+      .map<AddVideoForAdminRequestParams['videos'][number]>((item) => {
+        return {
+          videoId: item.videoId,
+          category: item.category,
+          level: item.levels.at(0) ?? 'BEGINNER',
+        };
+      });
+    if (videos.length > 0) {
+      mutate({ accessToken, params: { videos } });
+    }
+  };
 
   return (
     <>
@@ -41,7 +79,7 @@ export function AddVideoDialogContentContainer() {
         <DialogPrimitive.Close asChild>
           <Button>취소</Button>
         </DialogPrimitive.Close>
-        <Button>저장</Button>
+        <Button onClick={handleSave}>저장</Button>
       </DialogFooter>
     </>
   );
