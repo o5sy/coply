e2e1ee26a809fa1805abae5b15b9ae61b76dacf1
o5sy <oosydev@gmail.com>
2024-08-16 13:24:19 +0900

    Feat: viewport meta tag 추가

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Head, Html, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="ko">
-      <Head title="Coply" />
+      <Head title="Coply">
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <body>
         <Main />
         <NextScript />
