9fe5d54b0f1c5c9d3c31d9064af80a6162945a55
o5sy <oosydev@gmail.com>
2024-08-20 11:56:35 +0900

    Feat: usePreventScroll 훅 추가

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -3,3 +3,4 @@ export * from './use-local-storage';
 export * from './use-debounce';
 export * from './use-is-logged-in';
 export * from './use-intersection-observer';
+export * from './use-prevent-scroll';
